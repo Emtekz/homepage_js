5c38ac7a0a411c4c3b47a6bf8cf407580fc0f1e5
Paul Reiter <reiterpaul02@gmail.com>
2023-02-08 13:28:24 +0100

    Add: Kommentare

--- Home/Header/nav.js
+++ Home/Header/nav.js
@@ -9,20 +9,25 @@ const navIcon = document.getElementById('nav-icon');
 let open = false;
 
 // ---- Funktion ----
+// Wenn auf das Icon gedrückt wird:
 navIcon.addEventListener('click', () => {
    
+    // Wenn die Navbar offen ist:
     if(open){
-        // Navbar schließen
+        // Navbar schließen + Änderung des Icons zu Default
         navbar.style.display = 'none';
         navIcon.src = "Home/Header/Graphics/Nav_Menu.svg";
 
+        // Ist Navbar offen? -> Nein
         open = false;
     }
+    // Wenn die Navbar geschlossen ist:
     else if(!open){
-        // Navbar öffnen
+        // Navbar öffnen + Änderung des Icons zu Schließen
         navbar.style.display = 'inline-table';
         navIcon.src = "Home/Header/Graphics/Nav_Close.svg";
 
+        // Ist Navbar offen? -> Ja
         open = true;
     }
 } 
--- Home/ImageCarousel/image_carousel.js
+++ Home/ImageCarousel/image_carousel.js
@@ -5,25 +5,30 @@
 const allPictures = document.getElementsByClassName('carousel-item');
 
 // Verzögerung + Zähler
-const delay = 6000;
+const delay = 6 * 1000;
 let count = 0;
 
 // ---- Funktion ----
 // Erstes Element wird Sichtbar
 allPictures[count].style.opacity = 1;
 
-// Intervall, in welchem die Bilder gewechselt werden
+// Intervall, in welchem die Bilder gewechselt werden (6s)
 setInterval(Carousel, delay);
 
 // Funktion: Bildwechsel
 function Carousel() {
+    // "Altes" Bild wird ausgeblendet
     allPictures[count].style.opacity = 0;
 
+    // Zähler wird erhöht
     count += 1;
 
+    // Zähler wird auf 0 gesetzt, wenn er größer als die Anzahl
+    // der Bilder ist
     if (count == allPictures.length){
         count = 0;
     }
     
+    // "Neues" Bild wird eingeblendet
     allPictures[count].style.opacity = 1;
 }
